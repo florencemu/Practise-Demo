6443344a24d6812c0a3c3177ddb60608d67e6809
florencemu <muliang29@yeah.net>
2019-04-28 16:43:28 +0800

    2019-04-28

---
+++ 单例模式.php
@@ -0,0 +1,21 @@
+/*
+三私一公  公共静态方法作为返回对象的接口，私有化静态属性用于存放唯一一个单例对象。私有化构造方法，私有化克隆方法保证只存在一个单例
+
+单例模式有两种方法可以获得新的对象，一个是克隆 另外就是序列化反序列化
+*/
+
+
+<?php
+
+class GetJxSetByInfoIdHandler{
+	private static $news;//申请一个私有的静态成员变量来保存该类的唯一实例
+	private function __construct(){}//声明私有的构造方法，防止类外部创建对象
+	public static function instance(){
+		if(!(self::$news instanceof self)){//声明一个静态公共方法，供外部获取唯一实例
+			self::$new = new self;		
+	}
+		return self::$new;
+}
+	private function __clone(){}//声明私有的克隆方法，防止对象被克隆
+
+}
